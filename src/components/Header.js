bde571cd5fa241be1111a6a4433b6eca1171ea3c
Ayo <bimboladxtreme@gmail.com>
2021-08-11 04:12:18 +0100

    styled header left

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,51 @@
 import React from "react";
 import styled from "styled-components";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
-	return <Div>the header</Div>;
+	return (
+		<Div>
+			<div className="header__left">
+				<img
+					src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+					alt=""
+				/>
+				<div className="header__search">
+					<SearchIcon />
+					<input type="text" />
+				</div>
+			</div>
+			<div className="header__right"></div>
+		</Div>
+	);
 }
 
 export default Header;
 
-const Div = styled.div``;
+const Div = styled.div`
+	.header__left {
+		display: flex;
+	}
+
+	.header__left > img {
+		object-fit: contain;
+		height: 40px;
+		margin-right: 10px;
+	}
+
+	.header__search {
+		padding: 10px;
+		display: flex;
+		align-items: center;
+		border-radius: 5px;
+		height: 22px;
+		color: gray;
+		background-color: #eef3f8;
+
+		input {
+			outline: none;
+			border: none;
+			background: none;
+		}
+	}
+`;
